19722c6052aee20c3daaebd9c40e1d7b336009cf
Sanghee Lee <sanghee911@hotmail.com>
2017-12-18 10:41:44 +0900

    added admin creation in init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python manage.py migrate && python manage.py runserver 0.0.0.0:8000
+python manage.py migrate && \
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'root123')" && \
+python manage.py runserver 0.0.0.0:8000
